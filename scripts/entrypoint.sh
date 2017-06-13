042c4c81a4f2280ea2afd8671e437f0035b8195d
hongchhe <hehongchuang@hotmail.com>
2017-06-13 15:46:42 +0800

    add the default admin username/password
    1. add the default admin username/password
    2. update the README.md

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #django-admin startproject uicloud
-${USER_HOME}/projects/uicloud/manage.py makemigrations polls
-${USER_HOME}/projects/uicloud/manage.py sqlmigrate polls 0001
-${USER_HOME}/projects/uicloud/manage.py migrate
-${USER_HOME}/projects/uicloud/manage.py runserver 0:8000
+python ${USER_HOME}/projects/uicloud/manage.py makemigrations polls
+python ${USER_HOME}/projects/uicloud/manage.py sqlmigrate polls 0001
+python ${USER_HOME}/projects/uicloud/manage.py migrate
+python ${USER_HOME}/projects/uicloud/manage.py createsuperuser --username django --email test@test.com --noinput
+python ${USER_HOME}/projects/uicloud/manage.py shell -c 'from django.contrib.auth.models import User;u=User.objects.get(username__exact="django");u.set_password("aaaa1111");u.save()'
+python ${USER_HOME}/projects/uicloud/manage.py runserver 0:8000
 
 tail -f /dev/null
