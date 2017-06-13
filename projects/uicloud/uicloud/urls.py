ab092bdcc2fe7bdae123cce5973babed6e9ddaae
郭晓敏 <gxm199272@163.com>
2017-06-13 18:25:35 +0800

    郭晓敏创建了启动的 app--dataCollection

--- projects/uicloud/uicloud/urls.py
+++ projects/uicloud/uicloud/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-#    url(r'^polls/', include('polls.urls')),
+   url(r'^polls/', include('polls.urls')),
 ]
 
 
