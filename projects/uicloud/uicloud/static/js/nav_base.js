491296930eee3126c03656639f8f650e9fe5dddb
郭晓敏 <gxm199272@163.com>
2017-08-23 09:35:29 +0800

    添加 对象拷贝和判断相等的方法

--- projects/uicloud/uicloud/static/js/nav_base.js
+++ projects/uicloud/uicloud/static/js/nav_base.js
@@ -123,4 +123,67 @@ Array.prototype.isHasObjects = function(keys,values){
 	return -1;
 }
 
+// 对象的深度拷贝
+function objectDeepCopy(source) { 
+	return  JSON.parse(JSON.stringify(source)); 
+}
+// 判断俩个对象是否相等,非对象也可以判断，这里会比较类型，1 和“1”比较会返回 false
+function equalCompare(objA, objB)
 
+{
+    if (typeof arguments[0] != typeof arguments[1])
+        return false;
+
+    //数组
+    if (arguments[0] instanceof Array)
+    {
+        if (arguments[0].length != arguments[1].length)
+            return false;
+        
+        var allElementsEqual = true;
+        for (var i = 0; i < arguments[0].length; ++i)
+        {
+            if (typeof arguments[0][i] != typeof arguments[1][i])
+                return false;
+
+            if (typeof arguments[0][i] == 'number' && typeof arguments[1][i] == 'number')
+                allElementsEqual = (arguments[0][i] == arguments[1][i]);
+            else
+                allElementsEqual = arguments.callee(arguments[0][i], arguments[1][i]);            //递归判断对象是否相等                
+        }
+        return allElementsEqual;
+    }
+    
+    //对象
+    if (arguments[0] instanceof Object && arguments[1] instanceof Object)
+    {
+        var result = true;
+        var attributeLengthA = 0, attributeLengthB = 0;
+        for (var o in arguments[0])
+        {
+            //判断两个对象的同名属性是否相同（数字或字符串）
+            if (typeof arguments[0][o] == 'number' || typeof arguments[0][o] == 'string')
+                result = eval("arguments[0]['" + o + "'] == arguments[1]['" + o + "']");
+            else {
+                //如果对象的属性也是对象，则递归判断两个对象的同名属性
+                //if (!arguments.callee(arguments[0][o], arguments[1][o]))
+                if (!arguments.callee(eval("arguments[0]['" + o + "']"), eval("arguments[1]['" + o + "']")))
+                {
+                    result = false;
+                    return result;
+                }
+            }
+            ++attributeLengthA;
+        }
+        
+        for (var o in arguments[1]) {
+            ++attributeLengthB;
+        }
+        
+        //如果两个对象的属性数目不等，则两个对象也不等
+        if (attributeLengthA != attributeLengthB)
+            result = false;
+        return result;
+    }
+    return arguments[0] == arguments[1];
+}
