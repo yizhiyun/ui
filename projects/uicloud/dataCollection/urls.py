3827a006e98c1e458dc6c1e1f9796fd9d6754bac
wanJunHao <1007300061@qq.com>
2017-08-30 11:07:49 +0800

    删除一个无用接口

--- projects/uicloud/dataCollection/urls.py
+++ projects/uicloud/dataCollection/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-from .gxmHandleClass import panelFileHandle
 # add an app_name to set the application namespace
 app_name = 'dataCollection'
 
@@ -10,7 +9,6 @@ urlpatterns = [
     url(r"^showAllDbOfPalt$", views.showAllDbOfPalt),
     url(r"^tablesOfaDB$", views.showAllTablesOfaDataBase),
     url(r"^tableFileds$", views.showTableFiledsOFaTable),
-    url(r"^uploadFile$", panelFileHandle.upload),
     url(r"^detailTableData$", views.showTableDetailDataOfFileds),
     url(r"^filterTable/(?P<modeName>\w+)$", views.filterTable),
     url(r"^dataBuildView$", views.dataBuildView.as_view())
