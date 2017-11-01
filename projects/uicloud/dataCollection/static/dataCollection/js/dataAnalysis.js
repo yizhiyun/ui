f879587ef13ef8f0561b4d4624ee4e6f41066bd8
郭晓敏 <gxm199272@163.com>
2017-11-01 19:38:57 +0800

    修改导航判定方式

--- projects/uicloud/dataCollection/static/dataCollection/js/dataAnalysis.js
+++ projects/uicloud/dataCollection/static/dataCollection/js/dataAnalysis.js
@@ -333,7 +333,7 @@ function getCurrentDidBuildDataTable(){
       type:"get",
       dataType:"json",
       contentType: "application/json; charset=utf-8",
-      success:function(data){
+      success:function(data){ 	
         if (data["status"] == "success") {
           $("#analysisContainer .leftSlide #dataSet .detailDataSetList li .didBuildTables  ul.tablesList").empty();
           for (var i = 0;i < data.results.length;i++) {
@@ -345,10 +345,11 @@ function getCurrentDidBuildDataTable(){
              $("#dataSet .detailDataSetList  li .didBuildTables").find(".didBuildImg").css("visibility","visible");
   
           bindEventToPerTable();
-          leftNavCount++;
-          getLeftNavHeight();
+         
 
-        } 
+        }
+         leftNavCount++;
+         getLeftNavHeight();
       } 
   });
 }
@@ -560,8 +561,6 @@ function expression_click_handle(){
     $("#prompt_message #data_success_content").removeClass().addClass("data_success_img");
     $("#loading_percentage").css("right","-10px");
     //构建数据成功隐藏构建数据弹窗--显示选择进入模块弹窗
-//  navBtnAbleAndDisablesaveHandle("navDashBoardViewBtn");
-//  pallasdaraFunctionNavBtnHandle();
 	dashBoradFunction_able();
     $("#build_upload").hide("blind",1000,function(){
       $(".rightConent #analysisContainer").css("visibility","hidden");
