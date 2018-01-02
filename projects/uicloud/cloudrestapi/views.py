9ff4e927796fbbd85f9b1c3f3d4dfe841f82038a
hongchhe <hehongchuang@hotmail.com>
2018-01-02 16:15:40 +0800

    remove the log info

--- projects/uicloud/cloudrestapi/views.py
+++ projects/uicloud/cloudrestapi/views.py
@@ -164,7 +164,6 @@ def getTableViaSpark(request, tableName, modeName):
                            "reason": data.replace("False", "", 1)}
                 return JsonResponse(failObj, status=400)
             elif data.startswith("{"):
-                logger.info(data)
                 data = json.loads(data)
                 data = addColType(curUserName, tableName, data)
 
